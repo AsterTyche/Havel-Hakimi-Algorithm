3e68aad988cf627d952d0d7e267b9ba75e4c23a1
Aster Tyche <54491562+AsterTyche@users.noreply.github.com>
2020-09-10 20:55:01 -0700

    Add files via upload

--- HHA.py
+++ HHA.py
@@ -1,12 +1,14 @@
 def hha(deg_seq):
-    deg_seq = sorted(list(deg_seq), reverse = True)
-    if sum(deg_seq)%2 or (len(deg_seq) == 1 and deg_seq[0] != 0):
+    deg_seq = sorted(list(deg_seq), reverse = True) # sorts it
+    if sum(deg_seq)%2 or (len(deg_seq) == 1 and deg_seq[0] != 0): #
         return False
     elif len(deg_seq)==0 or deg_seq == [0]:
         return True
     else:
         deg = deg_seq[0]
         new = deg_seq[1:]
+        if deg > len(new):
+            return False
         for x in range(deg):
             new[x] = new[x]-1
         return hha(new)
